666c59766f4f524205aa90eb920d797718d49ca5
Luis Freddy <fdy7493@gmail.com>
2025-09-03 20:33:36 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ function Footer() {
                     <a href="https://wa.me/+573182327489" target="_blank" rel="noopener noreferrer">
                         <FaWhatsapp className="text-pink-500 text-2xl hover:scale-110 transition-transform" />
                     </a>
-                    <a href="https://www.tiktok.com/@fultonibarguenm" target="_blank" rel="noopener noreferrer">
+                    <a href="https://www.tiktok.com/@dudas.y.aportes?_t=ZS-8zMhKRj3nYw&_r=1">
                         <FaTiktok className="text-pink-500 text-2xl hover:scale-110 transition-transform" />
                 </a>
                 <p>Animate mira nuestras redes¡</p>
